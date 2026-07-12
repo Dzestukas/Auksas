58028a2b1ebf9988258111a74bf24beb84b22b26
Dzestukas <Jakubauskas0313@gmail.com>
2026-07-12 19:04:46 +0200

    Create chart.py

---
+++ chart.py
@@ -0,0 +1,64 @@
+import plotly.graph_objects as go
+
+
+def create_gold_chart(data):
+
+    fig = go.Figure()
+
+
+    # Žvakės
+    fig.add_trace(
+        go.Candlestick(
+            x=data.index,
+            open=data["Open"],
+            high=data["High"],
+            low=data["Low"],
+            close=data["Close"],
+            name="Gold"
+        )
+    )
+
+
+    # EMA 50
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["EMA_50"],
+            line=dict(width=2),
+            name="EMA 50"
+        )
+    )
+
+
+    # EMA 200
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["EMA_200"],
+            line=dict(width=2),
+            name="EMA 200"
+        )
+    )
+
+
+    # VWAP
+    if "VWAP" in data.columns:
+
+        fig.add_trace(
+            go.Scatter(
+                x=data.index,
+                y=data["VWAP"],
+                line=dict(width=2),
+                name="VWAP"
+            )
+        )
+
+
+    fig.update_layout(
+        height=600,
+        xaxis_rangeslider_visible=False,
+        template="plotly_dark"
+    )
+
+
+    return fig
