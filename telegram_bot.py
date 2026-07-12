a613526d375be382f029c225898bd7b644b8660a
Dzestukas <Jakubauskas0313@gmail.com>
2026-07-12 16:03:08 +0200

    Implement send_telegram_message function
    Added a function to send messages to Telegram using the Telegram Bot API.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import requests
 
+
+def send_telegram_message(message):
+
+    try:
+        token = st.secrets["TELEGRAM_TOKEN"]
+        chat_id = st.secrets["TELEGRAM_CHAT_ID"]
+
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown"
+        }
+
+        response = requests.post(url, json=payload)
+
+        return response.json()
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
